e111500c2d97e8a14c488088f02aaf829c4a2402
Nick Nallick <nick@purgatorydesign.com>
2020-05-20 16:56:53 -0600

    Add CustomDebugStringConvertible conformance

--- Sources/LinkedList/CustomStringConvertible.swift
+++ Sources/LinkedList/CustomStringConvertible.swift
@@ -4,7 +4,9 @@
 //  Copyright © 2020 Purgatory Design. Licensed under the MIT License.
 //
 
-extension LinkedList: CustomStringConvertible {
+extension LinkedList: CustomStringConvertible, CustomDebugStringConvertible {
 
     @inlinable public var description: String { Array(self).description }
+
+    @inlinable public var debugDescription: String { self.description }
 }
